37118598a2bdb2563523832c17f90bfc527171e9
Javier Etxarri <javier.etxarri@openbravo.com>
2024-10-21 06:54:14 +0200

    Initial Commit

---
+++ BlazeWSC/BlazeWSCApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlazeWSCApp.swift
+//  BlazeWSC
+//
+//  Created by Javier Etxarri on 21/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BlazeWSCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlazeWSC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlazeWSC
+//
+//  Created by Javier Etxarri on 21/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
